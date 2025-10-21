d06b706577c0f4fc45339ad09cb72b2f17110f30
kasm01 <86167934+kasm01@users.noreply.github.com>
2025-10-21 23:08:01 +0300

    Create _init_.py

---
+++ deployment/_init_.py
@@ -0,0 +1,6 @@
+# deployment/__init__.py
+"""
+Deployment package for Binance1-Pro Bot
+---------------------------------------
+Bu paket, botu Google Cloud ve Docker üzerinde deploy etmek için gerekli yapılandırmaları içerir.
+"""
